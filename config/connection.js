dadae79d9f3e24520a9bd3b2c63dd97469892b98
awoods1893 <awoods1983@gmail.com>
2022-09-05 12:14:56 -0500

    added .gitignore
    added gitignore and configured the mysql connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT
+  });
+
+module.exports = sequelize;
