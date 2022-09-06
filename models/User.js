6a50b5e28849645763b620840ac23367af51d1b4
awoods1893 <awoods1983@gmail.com>
2022-09-06 10:26:24 -0500

    Update User.js
    finish the user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,11 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model{}
+class User extends Model{
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
 
 
 User.init(
@@ -23,4 +27,14 @@ User.init(
           len: [4]
         }
       }
-    }
+    },
+    {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'User'
+  },
+);
+
+module.exports = User
