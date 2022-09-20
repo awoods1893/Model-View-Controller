fac7b2363b60152d4a8b78b242f58b1ba8d983a2
awoods1893 <awoods1983@gmail.com>
2022-09-20 18:27:12 -0500

    update port information

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.PORT,
   });
 
 module.exports = sequelize;
